55f833b1bd0df8a65bce6f55d8eccf639d875c29
Satyam <2018kucp1121@iiitkota.ac.in>
2021-10-21 17:02:35 +0530

    Final Commit

--- app/src/main/java/com/secure/quotiva/MainActivity.java
+++ app/src/main/java/com/secure/quotiva/MainActivity.java
@@ -1,27 +1,48 @@
 package com.secure.quotiva;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    MainViewModel mainViewModel;
+    TextView quoteText, quoteAuthor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        mainViewModel = new ViewModelProvider(this, new MainViewModelFactory(getApplication())).get(MainViewModel.class);
+        setQuote(mainViewModel.getQuote());
+    }
+
+    public void init(){
+        quoteText = findViewById(R.id.quoteText);
+        quoteAuthor = findViewById(R.id.quoteAuthor);
+    }
+
+    public void setQuote(Quote quote){
+        quoteText.setText(quote.text);
+        quoteAuthor.setText(quote.author);
     }
 
     public void onNext(View v){
-
+        setQuote(mainViewModel.nextQuote());
     }
 
     public void onPrevious(View v){
-
+        setQuote(mainViewModel.previousQuoto());
     }
 
     public void onShare(View v){
-
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, mainViewModel.getQuote().text);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/secure/quotiva/MainViewModel.java
+++ app/src/main/java/com/secure/quotiva/MainViewModel.java
@@ -1,12 +1,17 @@
 package com.secure.quotiva;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 
 import androidx.lifecycle.ViewModel;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class MainViewModel extends ViewModel {
@@ -14,23 +19,45 @@ public class MainViewModel extends ViewModel {
     private int index = 0;
     Context context;
 
+//    public MainViewModel(){
+//
+//    }
+
     MainViewModel(Context context){
         this.context = context;
-    }
-
-
-    void init(){
-         quoteList = loadQuoteFromAssets();
+        quoteList = loadQuoteFromAssets();
     }
 
     private ArrayList<Quote> loadQuoteFromAssets() {
         try {
-            InputStream inputStream = context.getAssets().open("quotes.json");
+            AssetManager assetManager = context.getAssets();
+            InputStream inputStream = assetManager.open("quotes.json");
             int size = inputStream.available();
-            //StringBuffer buffer = new ByteArrayInputStream();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            String json = new String(buffer, StandardCharsets.UTF_8);
+            Gson gson = new Gson();
+            ArrayList<Quote> quotes = gson.fromJson(json, new TypeToken<ArrayList<Quote>>(){}.getType());
+            return quotes;
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    Quote getQuote(){
+        int size = quoteList.size();
+        return quoteList.get(index);
+    }
+    Quote nextQuote(){
+        int size = quoteList.size();
+        index = (index + 1)%size;
+        return quoteList.get(index);
+    }
+    Quote previousQuoto(){
+        int size = quoteList.size();
+        index = (size + (index-1))%size;
+        return quoteList.get(index);
     }
 }
